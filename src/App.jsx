6fe40116b86d0dc578c166390f82add82bb28049
Md Noble <mdnoble.wd@gmail.com>
2024-03-23 20:09:36 +0600

    created react project and installed tailwind css and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-center text-purple-600">
+        Simple Dark and Light Mode
+      </h1>
+    </>
+  );
+}
+
+export default App;

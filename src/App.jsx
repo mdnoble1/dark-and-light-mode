2f65a511326363f2b55232266390ed1db75d9c8d
Md Noble <mdnoble.wd@gmail.com>
2024-03-23 20:35:57 +0600

    added three cards in ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <section>
       <Navbar></Navbar>
+      <Cards></Cards>
     </section>
   );
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,64 @@
+const Cards = () => {
+  return (
+    <section className="max-w-screen-2xl mx-auto py-32">
+      <div className="flex justify-center items-center gap-20">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/Ypngzwk/game1.png" alt="image" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Gaming Console</h2>
+            <p>
+              {" "}
+              Gaming consoles are specialized electronic devices designed
+              primarily for playing video games. They typically connect to a
+              television or monitor and use controllers to interact with the
+              games.
+            </p>
+            <div className="card-actions justify-center my-4">
+              <button className="btn btn-primary">Play Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/Ypngzwk/game1.png" alt="image" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Gaming Console</h2>
+            <p>
+              {" "}
+              Gaming consoles are specialized electronic devices designed
+              primarily for playing video games. They typically connect to a
+              television or monitor and use controllers to interact with the
+              games.
+            </p>
+            <div className="card-actions justify-center my-4">
+              <button className="btn btn-primary">Play Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/Ypngzwk/game1.png" alt="image" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Gaming Console</h2>
+            <p>
+              {" "}
+              Gaming consoles are specialized electronic devices designed
+              primarily for playing video games. They typically connect to a
+              television or monitor and use controllers to interact with the
+              games.
+            </p>
+            <div className="card-actions justify-center my-4">
+              <button className="btn btn-primary">Play Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cards;
